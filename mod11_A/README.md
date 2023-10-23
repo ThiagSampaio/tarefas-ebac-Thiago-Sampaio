c6df66ce5b5b7f3d894c2f4a4b870f8a1a26d41e
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2023-10-23 18:04:54 -0300

    Sobe primeira parte mod 11

---
+++ mod11_A/README.md
@@ -0,0 +1,14 @@
+# Tarefa ebac módulo 11-A
+
+
+Abaixo o print da Saída:
+
+<img src="prints/1.png">
+
+## Resposta Exercício.
+
+[Resposta](https://github.com/ThiagSampaio/tarefas-ebac-Thiago-Sampaio/blob/main/mod9/src/application/Program.java)
+
+Abaixo o print da Saída:
+
+<img src="prints/2.png">
---
+++ mod11_A/src/application/Program.java
@@ -0,0 +1,34 @@
+package application;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int condition = 1;
+		while(condition ==1) {
+			
+			System.out.println("Digite nomes separados por ',': ");
+			String listaNomes = sc.nextLine();
+			
+			
+			if(listaNomes.contains("-") || listaNomes.contains(";")||listaNomes.contains(":")||listaNomes.contains("/")) {
+				System.out.println("Uso de operador não autorizado");
+			}
+			else {
+			 listaNomes = listaNomes.replaceAll("\\s", "");
+			 String[]nomes = listaNomes.split(",");
+			 for(int i=0;i<nomes.length;i++) {
+				 System.out.println(nomes[i]);
+			 }
+			}
+			System.out.println("Deseja continuar o programa? (1) SIM , (-1) NÃO");
+			int c = sc.nextInt();
+			condition = c;
+			sc.nextLine();
+		}
+		sc.close();
+		
+	}
+
+}
